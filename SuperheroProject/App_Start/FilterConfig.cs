ebef235b3f71af2e1da7f5fba4c462c498275854
oliver.ryan9008 <38868314+rdo1708@users.noreply.github.com>
2018-07-24 13:24:20 -0500

    First commit

---
+++ SuperheroProject/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SuperheroProject
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ SuperheroProject/Models/Superhero.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SuperheroProject.Models
+{
+    public class Superhero
+    {
+        [Key]
+        public string SuperheroName { get; set; }
+        public string AlterEgo { get; set; }
+        public string PrimaryAbility { get; set; }
+        public string SecondaryAbility { get; set; }
+        public string Catchphrase { get; set; }
+
+    }
+}
