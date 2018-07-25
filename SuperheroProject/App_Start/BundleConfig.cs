e741becca6ac1448b979390b4d6fadcbb2dd0149
oliver.ryan9008 <38868314+rdo1708@users.noreply.github.com>
2018-07-25 14:18:42 -0500

    Added bootstrap theme. Moving on to customization and layout.

--- SuperheroProject/App_Start/BundleConfig.cs
+++ SuperheroProject/App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace SuperheroProject
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/superherobootstrap.css"));
         }
     }
 }
